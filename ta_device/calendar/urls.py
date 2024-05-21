72f0e3b42ff050f41dc09fd99acddd5ae5303dd3
Monir <monirulslm7@gmail.com>
2024-05-21 13:06:27 +0600

    holiday calendar and holiday added.

--- ta_device/calendar/urls.py
+++ ta_device/calendar/urls.py
@@ -1,5 +1,14 @@
-from django.urls import path, include
+from django.urls import path, include 
+from ta_device.calendar.views import (HolidayCreateView, HolidayListView, HolidayUpdateView, 
+                                    HolidayCalendarCreateView, HolidayCalendarListView, HolidayCalendarUpdateView)
+
 
 urlpatterns = [
-    # path('', HomePageView.as_view(), name='home'),
+    path('create/', HolidayCalendarCreateView.as_view(), name='calendar_create'),
+    path('update/<int:pk>', HolidayCalendarUpdateView.as_view(), name='calendar_update'),
+    path('all-list/', HolidayCalendarListView.as_view(), name='all_calendar_list'),
+    # HOLIDAY
+    path('holiday/create/', HolidayCreateView.as_view(), name='holiday_create'),
+    path('holiday/update/<int:pk>', HolidayUpdateView.as_view(), name='holiday_update'),
+    path('holiday/all-list/', HolidayListView.as_view(), name='all_holiday_list'),
 ]
